297e8356107102c63fa26994c64c40ee82c2919c
Семён <karachinstar@yandex.ru>
2024-09-03 23:04:30 +0800

    hw4

---
+++ hw4/src/main/java/org/example/Interfaces/ICarrierRepo.java
@@ -0,0 +1,16 @@
+package org.example.Interfaces;
+
+import org.example.Models.Carrier;
+
+/**
+ * Интерфейс взаимодействия с базой перевозчиков
+ */
+public interface ICarrierRepo {
+    /**
+     * Получить модель перевозчика из базы по ID
+     *
+     * @param id идентификатор перевозчика в базе
+     * @return
+     */
+    Carrier read(int id);
+}
---
+++ hw4/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.ClientApplication.Start;
+import org.example.Services.CashRepository;
+
+public class Main {
+    public static void main(String[] args) {
+        Start start = new Start();
+        start.runLoginRegisterMenu();;
+    }
+}
